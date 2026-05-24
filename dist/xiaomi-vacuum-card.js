8ec648c90638d6eb538a63473a676a6e10a96b1f
Yuris Auzins <zuz666@users.noreply.github.com>
2026-05-24 22:24:39 +0500

    chore: bump card version to 4.6.0

--- dist/xiaomi-vacuum-card.js
+++ dist/xiaomi-vacuum-card.js
@@ -1,6 +1,6 @@
 ((LitElement) => {
     console.info(
-        '%c XIAOMI-VACUUM-CARD %c 4.5.0 ',
+        '%c XIAOMI-VACUUM-CARD %c 4.6.0 ',
         'color: cyan; background: black; font-weight: bold;',
         'color: darkblue; background: white; font-weight: bold;',
     );
